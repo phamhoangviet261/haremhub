e51ee37c5b2eb5e97ddb063b6bc47fb271a66374
LAP12400\LAP12400-local <hoangviet.it.hcmus@gmail.com>
2022-09-15 17:40:54 +0700

    bcrypt error with vercel

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
  // DO NOT USE IT
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken')
 
 const User = require('../models/User')
