e939b7a55c3232950c2ed97abbe8db230dad34ab
LAP12400\LAP12400-local <hoangviet.it.hcmus@gmail.com>
2022-09-26 15:14:06 +0700

    server: https://haremhub-heroku-api.herokuapp.com

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,3 @@
-export const SERVER = 'https://haremhub-api.vercel.app'
+// export const SERVER = 'https://haremhub-api.vercel.app'
+export const SERVER = 'https://haremhub-heroku-api.herokuapp.com'
 // export const SERVER = 'http://localhost:5000'
--- server/README.js
+++ server/README.js
@@ -1,19 +1,5 @@
-let rating = {
-    "avgRating": 0
-}
+```heroku subtree push --prefix server master```
+> server: server folder 
+> master: master branch
 
-const miniComment = {
-    "rating": 5,
-    "comment": {
-        "idUserComment": "123456",
-        "content": "Good",
-        "createAt": new Date()
-    },
-    "react": [{
-        "type": "love",
-        "idUserReact": "654321",
-        "nameUserReact": "Shiro"
-    }]
-}
-
-let comment = [miniComment]
+```heroku config:set env_variable:value```
